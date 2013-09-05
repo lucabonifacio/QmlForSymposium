7a482aa21495ace9e9205918fae1e851b7fdd90a
lucab0ni <luca@bonifacio.ch>
2013-09-05 05:36:08 -0700

    Trying to fix problem with pushing content from travis to gh-pages

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -3,18 +3,17 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
 
-  cd $HOME/build/lucab0ni/QmlForSymposium
-  ls
+  cd $HOME/build/lucab0ni
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lucab0ni/QmlForSymposium.git gh-pages > /dev/null
 
   cd gh-pages
-  echo -e "gh-pages content before copy screens\n"
-  ls -la
-  cp -Rf $HOME/screens/* .
-  ls -la
-  echo -e "gh-pages content after to have copied screens\n"
+  echo -e "gh-pages content before copy screens"
+  ls -la screeens/
+  cp -Rf $HOME/screens/* screeens/
+  ls -la screeens/
+  echo -e "gh-pages content after to have copied screens"
 
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
