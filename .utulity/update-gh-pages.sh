80931fafe67569eebcaae8f4eb9acdfc8d2d824d
lucab0ni <luca@bonifacio.ch>
2013-09-05 02:32:48 -0700

    Trying to fix problem with pushing content from travis to gh-pages

--- .utulity/update-gh-pages.sh
+++ .utulity/update-gh-pages.sh
@@ -1,15 +1,13 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 echo -e "Starting to update gh-pages\n"
 
-  cp -R coverage $HOME/coverage
-
-  cd $HOME
+  cd $HOME/build/lucab0ni/QmlForSymposium
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lucab0ni/QmlForSymposium.git gh-pages > /dev/null
 
   cd gh-pages
-  cp -Rf $HOME/coverage/* .
+  cp -Rf $HOME/screens/* .
 
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
