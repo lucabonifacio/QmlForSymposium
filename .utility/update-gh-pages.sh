c79e353a66bd12f2d1f840066c30856cc14df39c
lucab0ni <luca@bonifacio.ch>
2013-09-05 05:47:28 -0700

    Trying to fix problem with screenshots taking

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -9,7 +9,6 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lucab0ni/QmlForSymposium.git gh-pages > /dev/null
 
   cd gh-pages
-  mkdir -p screens
   echo -e "gh-pages content before copy screens"
   ls -la screens/
   cp -Rf $HOME/screens/* screens/
